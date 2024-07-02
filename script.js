69fa5844303a3445e49b46e41a8476479abf3b5f
David <david.grigorian777@gmail.com>
2024-07-02 22:18:32 +0400

    yes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.getElementById('menu-btn');
-    const navLinks = document.querySelector('.nav__links');
+const burgerBtn = document.querySelector('.nav__burger-btn');
+const navLinks = document.querySelector('.nav__links');
+const header = document.querySelector('.header');
 
-    menuBtn.addEventListener('click', () => {
-            navLinks.classList.toggle('open');
-            menuBtn.classList.toggle('open');
-        });
-});
+
+function toggleMenu() {
+    navLinks.classList.toggle('open');
+    header.classList.toggle('open');
+}
+
+burgerBtn.addEventListener('click', toggleMenu);
